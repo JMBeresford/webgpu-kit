1ad439f0442377385e9c395ce037618ec0efa858
John Beresford <jberesford@volcaus.com>
2024-05-09 14:29:54 -0700

    fix(docs): ssg

--- apps/web/app/docs/[type]/[id]/page.tsx
+++ apps/web/app/docs/[type]/[id]/page.tsx
@@ -20,7 +20,7 @@ const views: Record<string, View | undefined> = {
 };
 
 export default function Page(props: { params: ViewParams }): JSX.Element {
-  const { type } = props.params;
+  const type = normalizeTypeName(props.params.type);
   const View = views[type];
 
   if (!View) {
@@ -37,6 +37,12 @@ export default function Page(props: { params: ViewParams }): JSX.Element {
 
 export function generateStaticParams(): ViewParams[] {
   return Array.from(allGroups)
-    .map(([type, ids]) => ids.map((id) => ({ type, id })))
+    .map(([type, ids]) =>
+      ids.map((id) => ({ type: normalizeTypeName(type), id })),
+    )
     .flat();
+}
+
+function normalizeTypeName(type: string): string {
+  return type.toLowerCase().replaceAll(" ", "");
 }
